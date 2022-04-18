c51f66a53d7a67cc641c05f078371cf1a2519350
Michael W Spalti <mspalti@gmail.com>
2022-04-18 16:07:06 -0700

    Some work on PDF procesing

--- existdb/fetchPdfFiles.py
+++ existdb/fetchPdfFiles.py
@@ -1,21 +1,18 @@
-import urllib
-from contextlib import contextmanager
 
+from urllib.request import urlopen
+import urllib.error
+from contextlib import contextmanager
 from existDbFields import ExistDbFields
+
 
 def write_pdf_to_contents(out_dir, file_name):
 
-    try:
-        # self.append_canvas_json(height, width, page_count)
-        # Add text file to the saf contents file.
-        with open(out_dir + '/contents', 'a') as contents:
-            # Add Alto to dspace bundle name OtherContent).
-            contents.write(file_name + '\n')
-            contents.close()
-
-    except IOError as err:
-        print('An error occurred writing contents to saf for: %s. See %s' % ('thumb.jpg', out_dir))
-        print('IO Error: {0}'.format(err))
+    # self.append_canvas_json(height, width, page_count)
+    # Add text file to the saf contents file.
+    with open(out_dir + '/contents', 'a') as contents:
+        # Add Alto to dspace bundle name OtherContent).
+        contents.write(file_name + '\n')
+        contents.close()
 
 
 class FetchPdfFiles:
@@ -35,16 +32,28 @@ class FetchPdfFiles:
         if not dry_run:
             self.fetch_file(file_name, collection, out_dir)
 
+    # fetch_file assumes that the PDF file is in the eXist-db. This isn't always true. When
+    # a file is not found this method will log a 404 error. Generally, this method should not be called
+    # for a collection with no  item-level PDF files.
     def fetch_file(self, file_name, collection, out_dir):
         URL = 'http://exist.willamette.edu:8080/exist/rest/db/' + collection + '/pdf/' + file_name
 
-        with self.closing(urllib.request.urlopen(URL)) as url:
-            with open(out_dir + '/' + file_name, 'wb') as f:
-                f.write(url.read())
-
         try:
+            with self.closing(urlopen(URL)) as url:
+                with open(out_dir + '/' + file_name, 'wb') as f:
+                    f.write(url.read())
+
             out_dir + '/' + file_name
             write_pdf_to_contents(out_dir, file_name)
-        except:
-            print('An error occurred writing to content file for %s: %s.' % (out_dir, URL))
-            self.analyzer.add_alto_processing_failed(out_dir + ': ' + URL)
+
+        except urllib.error as err:
+            print('An error occurred fetching file for %s: %s.' % (URL, err))
+            self.analyzer.add_pdf_processing_failed(out_dir + ': ' + URL)
+
+        except IOError as err:
+            print('An error occurred writing contents to saf for: %s. See %s' % ('thumb.jpg', out_dir))
+            print('IO Error: {0}'.format(err))
+            self.analyzer.add_pdf_processing_failed(out_dir + ': ' + URL)
+
+
+
